3c6f98ac292946a16527d698129855c9666b35f9
Radek Pištělák <radek.pistelak@kiwi.com>
2020-02-05 15:04:44 +0100

    intial commit
    - simple card component
    - known issues: background color is not set based on isActive

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <Card header="Key" title="Used" content="Description" isActive></Card>
+          <Card header="Key" title="Used" content="Description"></Card>
+          <Card header="Key" title="Used" content="Description"></Card>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Card from "./components/card.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Card
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/card.vue
@@ -0,0 +1,29 @@
+<template>
+  <div v-bind:class="appearence" class="card text-white mb-3" style="max-width: 18rem;">
+    <div class="card-header">{{header}}</div>
+    <div class="card-body">
+      <h5 class="card-title">{{title}}</h5>
+      <p class="card-text">{{content}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: {
+    header: String,
+    title: String,
+    content: String,
+    isActive: Boolean
+  },
+  computed: {
+    appearence: function() {
+      return this.isActive ? "bg-success" : "bg-danger"
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
